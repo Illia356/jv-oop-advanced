ec9c4befb89df0a412c63205467ca620077b96ba
Illia356 <illyarudenko077@gmail.com>
2026-08-24 22:47:13 +0300

    Fin3

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,25 +3,26 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
+    public static final int FIGURE_COUNT = 5;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
-        int figureType = random.nextInt(5);
+        int figureType = random.nextInt(FIGURE_COUNT);
         String color = colorSupplier.getRandomColor();
+        int parameter = random.nextInt(10) + 1;
 
         switch (figureType) {
             case 0:
-                return new Circle(color, random.nextInt(10) + 1);
+                return new Circle(color, parameter);
             case 1:
-                return new RightTriangle(color, random.nextInt(10) + 1, random.nextInt(10) + 1);
+                return new RightTriangle(color, parameter, parameter);
             case 2:
-                return new Rectangle(color, random.nextInt(10) + 1, random.nextInt(10) + 1);
+                return new Rectangle(color, parameter, parameter);
             case 3:
-                return new Square(color, random.nextInt(10) + 1);
+                return new Square(color, parameter);
             case 4:
-                return new IsoscelesTrapezoid(color, random.nextInt(10) + 1,
-                        random.nextInt(10) + 1, random.nextInt(10) + 1);
+                return new IsoscelesTrapezoid(color, parameter, parameter, parameter);
             default:
                 return getDefaultFigure();
         }
